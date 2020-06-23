44f4805df4faf10284661cc1ca38c242703a4bc3
Esteban Federico Hernandez Losada <esteban.losada@wnco.com>
2020-06-23 17:13:15 -0500

    Usage of Ribbon and Feign to avoid hard-coding the URL to be consumed and rely on Eureka for that

--- testob-ping-service/src/main/java/com/eh/testob/testobpingservice/proxies/PongServiceProxy.java
+++ testob-ping-service/src/main/java/com/eh/testob/testobpingservice/proxies/PongServiceProxy.java
@@ -1,13 +1,15 @@
 package com.eh.testob.testobpingservice.proxies;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@FeignClient(name = "testob-pong-service", url = "localhost:8100")
+@FeignClient(name = "testob-api-gateway")
+@RibbonClient(name = "testob-pong-service")
 public interface PongServiceProxy {
 
-   @GetMapping(path = "/pong")
+   @GetMapping(path = "/testob-pong-service/pong")
    public String retrievePongMessage();
 
 }
